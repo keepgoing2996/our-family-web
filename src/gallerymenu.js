1bd9c666c86e6574fd7cfd11dbee6e450d0130b6
Zhaoting Ye <zhaoting_ye@apple.com>
2022-12-11 23:01:05 -0800

    add our family app

---
+++ src/gallerymenu.js
@@ -0,0 +1,23 @@
+const gallerymenu = [
+    {
+        path: "/images/IMG_2533.JPG",
+        alt: "sunshine",
+    }, 
+    {
+        path: "/images/IMG_2534.JPG",
+        alt: "vehicle"
+    },
+    {
+        path: "/images/IMG_2535.JPG",
+        alt: "beach"
+    },
+    {
+        path: "/images/IMG_2536.JPG",
+        alt: "plant"
+    },
+    {
+        path: "/images/IMG_2537.JPG",
+        alt: "sky"
+    },
+];
+export default gallerymenu;
